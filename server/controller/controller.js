b269bfc72fe3aa976e4071469ae7d36b63ab6fa7
David Leigh <daleighan@gmail.com>
2017-09-22 02:03:06 -0700

    Made the deleteAllByUser function work correctly

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,50 +3,20 @@ const DB = require('../../DB/db.js');
 
 module.exports.getMessagesByUser = (req, res) => {
   const username = req.path.substr(req.path.lastIndexOf('/') + 1);
-  DB.User.findAll({
-  	where: { username: username }
-  }).then((person) => {
-  	DB.Message.findAll({
-  		where: { user_id: person[0].id }
-  	})
-  	.then((results) => {
-  		console.log();
-  		res.status(200).json(results);
-  	})
-  	.catch((err) => {
-  		res.status(404).json(err);
-  	});
-  });
+  
 };
 
 module.exports.deleteAllByUser = (req, res) => {
 	const username = req.path.substr(req.path.lastIndexOf('/') + 1);
 	let userIdDelete;
-  DB.User.findAll({
+  DB.User.destroy({
   	where: { username: username }
-  })
-  .then((user) => {
-  	userIdToDelete = user;
-  }).then((user) => {
-  	DB.User.destroy({
-  		where: {
-  			username: username
-  		}
-  	}).then((user) => {
-  		DB.Message.destroy({
-  			where: user.id
-  		}).then((user) => {
-  			DB.Conversation.destroy({
-  				where: user.id
-  			}).then((data) => {
-  				res.status(204).json(data)
-  			}).catch((err) => {
-  				res.status(404).json(err);
-  			})
-  		})
-  	})
-  })
-}
+  }).then((destroyed) => {
+  	res.json(destroyed);
+  }).catch((err) => {
+  	res.status(404).json(err);
+  });
+};
 
 module.exports.addProfile = (req, res) => {
 	DB.User.create({
@@ -58,7 +28,7 @@ module.exports.addProfile = (req, res) => {
 	}).catch((err) => {
 		res.status(404).json(err);
 	});
-}
+};
 
 
 
