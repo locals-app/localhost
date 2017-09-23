550fdac59066df0fb98a09433aa1e7ed22c53758
thismax <max.g.jacobs@gmail.com>
2017-09-23 16:44:24 -0700

    Profile Skeleton Complete

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import App from './components/app';
+=======
+import Profile from './components/Profile';
+>>>>>>> Profile Skeleton Complete
 
-ReactDOM.render(<App />, document.getElementById('app')); 
+ReactDOM.render(<Profile />, document.getElementById('app')); 
