94eaef70c51757e63edf6e3f93b7bdf92e0bc361
thismax <max.g.jacobs@gmail.com>
2017-09-23 10:57:45 -0700

    commiting current skeleton before merging

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ConversationStubs from './ConversationStubs';
+import ConversationStubs from './ConversationStubs.jsx';
 
 
 class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       userLocationSet: false,
     }
   }
 
+
   render() {
 
-    if (this.state.userLocationSet) {
+    return (<div>hi</div>)
+
+    // if (this.state.userLocationSet) {
       
-      return (
-        <div>
-          <input type="text" name="whereTo"/>
-          <h1>Ongoing Conversations</h1>
-          <ConversationStubs />
-        </div>
-      )
-    } 
+    //   return (
+    //     <div>
+    //       <input type="text" name="whereTo"/>
+    //       <h1>Ongoing Conversations</h1>
+    //       {/* <ConversationStubs /> */}
+    //     </div>
+    //   )
+    // } 
     
-    else {
-      return (
-        <div> Set your location </div>
-      )
-    }
+    // else {
+    //   return (
+    //     <div> Set your location </div>
+    //   )
+    // }
 
   }
   
