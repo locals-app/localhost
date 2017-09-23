eb6fc62f4f28466e4569dad99e6a02c03028bce5
thismax <max.g.jacobs@gmail.com>
2017-09-23 12:31:41 -0700

    Locals skeleton complete

--- client/src/components/Locals.jsx
+++ client/src/components/Locals.jsx
@@ -1,1 +1,52 @@
 // list of all locals
+import React, { Component } from 'react';
+import axios from 'axios';
+import Local from './Local'
+
+class Locals extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      locals: [],
+      ready: false,
+    };
+    this.getLocals = this.getLocals.bind(this);
+  }
+
+  getLocals (location) {
+    location = location.split(' ').join('_');
+    axios.get(`/api/${location}`) //might need to do something w/ React Router to make this work;
+      .then( (results) => {
+        this.setState({
+          locals: results.data,
+          ready: true,
+        });
+      })
+      .catch( (err) => {
+        console.log(err)
+      });
+  }
+
+  render() {
+    if (this.state.ready) {
+      return (
+        <div>
+          {
+            this.state.locals.map((local) => {
+              return <Local local={local} />
+            })
+          }
+        </div>
+      )
+    } else {
+      this.getLocals('Los Angeles'); // this needs to be changed to handle a click at the App level and route to this component.
+      return (
+        <div>
+          Loading       
+        </div>
+      )
+    }
+  }
+}
+
+export default Locals;
