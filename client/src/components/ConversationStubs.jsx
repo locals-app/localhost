6d87781dc106b8a61c007e0f2187ae74050378ff
thismax <max.g.jacobs@gmail.com>
2017-09-23 10:33:28 -0700

    init

---
+++ client/src/components/ConversationStubs.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const ConversationStubs = (props) => {
+  render() {
+    return (
+      <div>
+        Here's  where one coversation stub would go.
+      </div>
+    )
+  }
+};
+
+export default ConversationStubs;
