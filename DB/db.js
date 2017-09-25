decdca3ea0789066bb3ebca181655f809184a0a5
David Leigh <daleighan@gmail.com>
2017-09-25 11:15:19 -0700

    Add some sample data. It now contains 3 conversations between max and each of the other users each with 3 messages.

--- DB/db.js
+++ DB/db.js
@@ -111,10 +111,10 @@ User.sync({ force: true }).then(() => {
           Conversation.sync({ force: true }).then(() => {
             return Conversation.create({
               firstUser: 1,
-              secondUser: 2
+              secondUser: 3
             }).then(() => {
               return Conversation.create({
-                firstUser: 1,
+                firstUser: 2,
                 secondUser: 3
               }).then(() => {
                 return Conversation.create({
@@ -123,19 +123,55 @@ User.sync({ force: true }).then(() => {
                 }).then(() => {
                   Message.sync({ force: true }).then(() => {
                     return Message.create({
-                      text: 'a new message',
+                      text: 'From Alex to Max',
                       userId: 1,
                       conversationId: 1
                     }).then(() => {
                       return Message.create({
-                        text: 'another new message',
-                        userId: 2,
+                        text: 'and Back',
+                        userId: 3,
                         conversationId: 1
                       }).then(() => {
                         Message.create({
-                          text: 'yet another one',
-                          userId: 3,
-                          conversationId: 3
+                          text: 'And back again',
+                          userId: 1,
+                          conversationId: 1
+                        }).then(() => {
+                          return Message.create({
+                            text: 'From Jeff to Max',
+                            userId: 2,
+                            conversationId: 2
+                          }).then(() => {
+                            return Message.create({
+                              text: 'and Back',
+                              userId: 3,
+                              conversationId: 2
+                            }).then(() => {
+                              Message.create({
+                                text: 'And back again',
+                                userId: 2,
+                                conversationId: 2
+                              }).then(() => {
+                                return Message.create({
+                                  text: 'From Tiffany to Max',
+                                  userId: 4,
+                                  conversationId: 3
+                                }).then(() => {
+                                  return Message.create({
+                                    text: 'and Back',
+                                    userId: 3,
+                                    conversationId: 3
+                                  }).then(() => {
+                                    Message.create({
+                                      text: 'And back again',
+                                      userId: 4,
+                                      conversationId: 3
+                                    });
+                                  });
+                                });
+                              })
+                            });
+                          });
                         });
                       });
                     });
