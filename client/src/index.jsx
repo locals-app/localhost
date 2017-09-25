7e9a4063b20d092b5e5476e9bcd5cf80b119e152
thismax <max.g.jacobs@gmail.com>
2017-09-25 09:53:50 -0700

    resolving

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './components/app';
-=======
-import Profile from './components/Profile';
->>>>>>> Profile Skeleton Complete
 
-ReactDOM.render(<Profile />, document.getElementById('app')); 
+ReactDOM.render(<App />, document.getElementById('app')); 
