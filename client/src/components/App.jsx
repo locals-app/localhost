6fded8b929f6e5ca71de8b160cd2af34a3d2b4c5
thismax <max.g.jacobs@gmail.com>
2017-09-27 12:51:17 -0700

    following tutorial, able to show lock

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,44 +9,59 @@ import Landing from './splash/Landing';
 import Callback from '../utils/Callback.jsx';
 
 class App extends Component {
+
+  componentWillMount() {
+    this.lock = new Auth0Lock('kaQTBjg6m1VWXujuWrjYNDahHpDyJBEk', 'localhost-app.auth0.com');
+  }
   
   render() {
-    return (
-      <div>
-        <div>
-          <div>
-            <BrowserRouter>
-              <div>
-                <Route path='/Home' render={() => (
-                  !isLoggedIn() ? 
-                    login()   
-                  (
-                    <div>
-                      <Callback />
-                      <Redirect to="/Home"/>
-                    </div>
-                  )
-                   : (
-                    <Redirect to="/MyProfile"/>
-                  )
-                )}/>
-                <Route path='/callback' component={Callback}/>
-                <Route path='/Locals' component={Locals} onEnter={login}/>
-                <Route path='/MyProfile' component={Profile} onEnter={requireAuth}/>
-                <Route path='/Logout' onEnter={logout}/>
-                <Route path='/Login' onEnter={login}/>
-                <li><NavLink to='/Home'>Home</NavLink></li>
-                <li><NavLink to='/Locals'>Locals</NavLink></li>
-                <li><NavLink to='/MyProfile'>My Profile</NavLink></li>
-                <li><NavLink to='/Logout'>Logout</NavLink></li>
-                <li><div onClick={()=>{console.log(isLoggedIn())}}> is it logged in? </div></li>
-              </div>
-            </BrowserRouter>
-          </div>
-        </div>
-      </div>
-    )
+    return <Landing lock={this.lock}/>
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <div>
+  //         <BrowserRouter>
+  //           <div>
+  //             <Route path='/Home' render={() => (
+  //               !isLoggedIn() ? 
+  //                 login()   
+  //               (
+  //                 <div>
+  //                   <Callback />
+  //                   <Redirect to="/Home"/>
+  //                 </div>
+  //               )
+  //                : (
+  //                 <Redirect to="/MyProfile"/>
+  //               )
+  //             )}/>
+  //             <Route path='/callback' component={Callback}/>
+  //             <Route path='/Locals' component={Locals} onEnter={login}/>
+  //             <Route path='/MyProfile' component={Profile} onEnter={requireAuth}/>
+  //             <Route path='/Logout' onEnter={logout}/>
+  //             <Route path='/Login' onEnter={login}/>
+  //             <li><NavLink to='/Home'>Home</NavLink></li>
+  //             <li><NavLink to='/Locals'>Locals</NavLink></li>
+  //             <li><NavLink to='/MyProfile'>My Profile</NavLink></li>
+  //             <li><NavLink to='/Logout'>Logout</NavLink></li>
+  //             <li><div onClick={()=>{console.log(isLoggedIn())}}> is it logged in? </div></li>
+  //           </div>
+  //         </BrowserRouter>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
