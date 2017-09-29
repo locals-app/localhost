2c335e7e27e68d9a53cd8b96e1b2b9e88bda01cb
thismax <max.g.jacobs@gmail.com>
2017-09-29 00:14:40 -0700

    removed authmiddleware

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 // import files
-const authMiddleware = require('./auth.middleware');
 const router = require('./router/router.js');
 const db = require('../DB/db.js');
 // instantiate server and socketIo
@@ -15,8 +14,6 @@ const server = http.createServer(app);
 const io = socketIo(server);
 const port = process.env.PORT || 3000;
 // middleware
-// app.use('/api', authMiddleware.jwtCheck);   // --> these checks are what will need to be figured out
-// app.use('/api', authMiddleware.readScope);    --> these checks are what will need to be figured out
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
