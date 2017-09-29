ee815b872bf209020b09f4387383c91de2a96d23
thismax <max.g.jacobs@gmail.com>
2017-09-29 16:03:14 -0700

    added un/pic/recent image to convo stub

--- client/src/components/splash/ConvoStub.jsx
+++ client/src/components/splash/ConvoStub.jsx
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class ConvoStub extends Component {
 	constructor(props) {
 		super(props);
 		this.state ={
 			otherUser: '',
+			otherUserImageUrl: '',
 		}
 
 		this.findOtherUser = this.findOtherUser.bind(this);
+		this.fetchOtherUserImage = this.fetchOtherUserImage.bind(this);
 	}
 
 	componentWillMount() {
-		this.setState({otherUser: this.findOtherUser(this.props.messages)});
+		this.findOtherUser();
+	}
+
+	componentDidMount() {
+		this.fetchOtherUserImage();
 	}
 
 	findOtherUser(messages) {
-		
+		this.props.messages.forEach( message => {
+			if (message.userId !== this.props.currentUser) {
+				this.setState({otherUser: message.userId})
+			}
+		});
+	}
+
+	fetchOtherUserImage() {
+		axios.get(`api/profiles/${this.state.otherUser}`).then((userData) => {
+			this.setState({otherUserImageUrl: userData.data.imageUrl});
+		}).catch(err => console.error(err));
 	}
 
 	render = () => {
 		let { messages, currentUser } = this.props;
-		console.log('====================================')
-		console.log(currentUser);
-		console.log('====================================')
 		return (
 			<div>
-				{messages.map((message, i) => {
-					return <div key={i}>{message.userId.replace('_', ' ')}: {message.text}</div>
-				})}
+				<span>Conversation with {this.state.otherUser.replace('_', ' ')}</span>
+				<img src={this.state.otherUserImageUrl} style={{width: 20}} alt=""/>
+				<div>
+					{messages[messages.length - 1].text}
+				</div>
 			</div>
 		)
 	}
